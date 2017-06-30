eb2ad659ba790db40fdbea4cd4464f8d039e0987
conesgnomes <connorlarsen@gmail.com>
2017-06-30 14:45:44 -0700

    get for loop working in scripts-interface.js for displaying each clinic, their email address, and phone number

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,7 +1,12 @@
 var Doctor = require('./../js/scripts.js').doctorModule;
 
-var clinics = function(clinics) {
-  $('.result').append("<p><strong>Offices: " + clinics + "</strong></p>");
+var clinicGroup = function(clinicArray) {
+  for (i = 0; i < clinicArray.length; i++) {
+    var clinicName = clinicArray.shift();
+    var clinicWebsite = clinicArray.shift();
+    var clinicPhone = clinicArray.shift();
+    $('.clinic-list').after('<h3>' + clinicName + '</h3><ul><li><strong>Website: </strong>' + clinicWebsite + '</li><li><strong>Phone: </strong>' + clinicPhone + '</li></ul>')
+  };
 };
 
 $(function() {
@@ -13,7 +18,7 @@ $(function() {
     searchInput = $('#issue-search').val();
     $('#issue-search').val('');
 
-    newDoctor.getDoctors(searchInput, clinics);
+    newDoctor.getDoctors(searchInput, clinicGroup);
 
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,33 @@ var apiKey = require('./../.env').apiKey;
 var Doctor = function() {
 };
 
-Doctor.prototype.getDoctors = function(medicalIssue, clinics) {
+Doctor.prototype.getDoctors = function(medicalIssue, clinicGroup) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
     .then(function(result) {
-      // var nameArray = [];
 
       for (var i = 0; i < result.data.length; i++) {
         for (var j = 0; j < result.data[i].practices.length; j++) {
+
+          //////////////////////////////////////////
+          // If website is undefined, display N/A
+          //////////////////////////////////////////
+
+          if (JSON.stringify(result.data[i].practices[j].website) === undefined) {
+              result.data[i].practices[j].website = "N/A";
+            }
+
+          //////////////////////////////////////////
+          // Gather data from API
+          //////////////////////////////////////////
+
           var names = result.data[i].practices[j].name
-          // nameArray.push(name);
-          clinics(names);
+          var sites = result.data[i].practices[j].website
+          var phone = result.data[i].practices[j].phones[0].number
+          var clinicArray = [];
+          clinicArray.push(names, sites, phone);
+          clinicGroup(clinicArray);
         }
       }
-      // console.log(nameArray);
     })
    .fail(function(error){
       console.log("fail");
